fe6fade5ea004b58750ff4038d9d5046bd6324d9
Алекс <@DESKTOP-LB5OKJL>
2020-02-27 21:10:28 +0300

    Load

---
+++ Triangle/Triangle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Triangles
+{
+    public enum KindTriangle
+    {
+        Acute,
+        Right,
+        Obtuse
+    }
+
+    public static class Triangle
+    {
+        public static KindTriangle GetKindTriangle(double a, double b, double c)
+        {
+            double max;
+            double min1;
+            double min2;
+
+            if ((a > b) && (a > c))
+            {
+                max = a;
+                min1 = b;
+                min2 = c;
+            }
+            else
+            {
+                min1 = a;
+
+                if (b > c)
+                {
+                    max = b;
+                    min2 = c;
+                }
+                else
+                {
+                    max = c;
+                    min2 = b;
+                }
+            }
+
+            if (min1 + min2 > max)
+            {
+                var result = Math.Pow(max, 2) - Math.Pow(min1, 2) - Math.Pow(min2, 2);
+                if (result < 0)
+                    return KindTriangle.Acute;
+                else if (result > 0)
+                    return KindTriangle.Obtuse;
+                else
+                    return KindTriangle.Right;
+            }
+            else
+                throw new Exception("Треугольника с такими сторонами не существует");
+        }
+    }
+}
---
+++ UnitTestTriangle/UnitTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Triangles;
+
+namespace UnitTestTriangle
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void AcuteTriangle()
+        {
+            Assert.AreEqual(KindTriangle.Acute, Triangle.GetKindTriangle(1, 2, 2));
+        }
+
+        [TestMethod]
+        public void RightTriangle()
+        {
+            Assert.AreEqual(KindTriangle.Right, Triangle.GetKindTriangle(5, 3, 4));
+        }
+
+        [TestMethod]
+        public void ObtuseTriangle()
+        {
+            Assert.AreEqual(KindTriangle.Obtuse, Triangle.GetKindTriangle(3, 6, 4));
+        }
+
+        [TestMethod]
+        public void ImpossibleTriangle()
+        {
+            Assert.ThrowsException<Exception>(() => Triangle.GetKindTriangle(3, 6, 3));
+        }
+
+        [TestMethod]
+        public void ImpossibleTriangleNegativeSide()
+        {
+            Assert.ThrowsException<Exception>(() => Triangle.GetKindTriangle(-1, -1, -1));
+        }
+    }
+}
